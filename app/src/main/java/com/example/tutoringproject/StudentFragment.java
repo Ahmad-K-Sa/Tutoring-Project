354779bad0c948ae6ee27bf36a7a73f863720b0b
Ahmad-K-Sa <Ahmad-K-Sa@users.noreply.github.com>
2020-11-25 17:44:27 +0200

    Fixed minor bug

--- app/src/main/java/com/example/tutoringproject/StudentFragment.java
+++ app/src/main/java/com/example/tutoringproject/StudentFragment.java
@@ -1,5 +1,8 @@
 package com.example.tutoringproject;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,51 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StudentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StudentFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StudentFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StudentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StudentFragment newInstance(String param1, String param2) {
-        StudentFragment fragment = new StudentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    SQLiteDatabase db;
+    Cursor cursor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +25,18 @@ public class StudentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SQLiteOpenHelper helper = new DatabaseSQLiteOpenHelper(getContext());
+        db = helper.getWritableDatabase();
+        cursor = db.query("TUTORS", new String[]{"_id", "USERNAME", "PASSWORD","COURSES","FIRSTNAME","LASTNAME"}, null, null, null, null, null);
+        if(cursor.moveToFirst()){
+            while(!cursor.isLast()){
+
+            }
+        }
     }
 }
