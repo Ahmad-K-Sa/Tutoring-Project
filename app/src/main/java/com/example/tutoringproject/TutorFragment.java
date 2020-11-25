3a09070cf0053bbcd1201ea90fe51e01542cd3b2
Reem Saado <reemsaado>
2020-11-25 18:08:35 +0200

    edit all

--- app/src/main/java/com/example/tutoringproject/TutorFragment.java
+++ app/src/main/java/com/example/tutoringproject/TutorFragment.java
@@ -7,52 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TutorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TutorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TutorFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TutorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TutorFragment newInstance(String param1, String param2) {
-        TutorFragment fragment = new TutorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +23,33 @@ public class TutorFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tutor, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        View.OnClickListener onclickSetSched = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).flipSetSchedule();
+
+            }
+        };
+        Button buttonSched = view.findViewById(R.id.setSchedule);
+
+        buttonSched.setOnClickListener(onclickSetSched);
+
+
+        View.OnClickListener onclickViewSched = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).flipViewSchedule();
+
+            }
+        };
+        Button buttonsched = view.findViewById(R.id.viewschedule);
+
+        buttonsched.setOnClickListener(onclickViewSched);
     }
 }
